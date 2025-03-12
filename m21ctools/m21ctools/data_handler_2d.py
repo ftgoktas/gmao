7548a7c42ea187f0a2faa8862dd460b179af62d2
Sabin Adhikari <39281480+sabin413@users.noreply.github.com>
2025-03-12 10:50:27 -0500

    Update data_handler_2d.py

--- m21ctools/m21ctools/data_handler_2d.py
+++ m21ctools/m21ctools/data_handler_2d.py
@@ -117,23 +117,3 @@ class CubedSphereData:
         plt.title('Specific Humidity on Latitude-Longitude Grid', fontsize=14)
         fig.savefig('specific_humidity.png')
         plt.show()
-
-# calculate
-
-#file_path = '../../gmao/data/e5303_m21c_jan18.lmv_inst_1hr_glo_C360x360x6_slv.2018-01-31T0000Z.nc4'
-#data_handler = CubedSphereData(file_path=file_path)
-
-#raw_data = data_handler.raw_data
-
-#clean_data = data_handler.raw_data_cleaned
-
-#print("Min:", np.nanmin(data_handler.data), "Max:", np.nanmax(data_handler.data))
-
-#lat_grid, lon_grid, data_grid = data_handler.interpolate_to_latlon_grid()
-
-#data_handler.plot_data(lat_grid, lon_grid, data_grid)
-
-
-
-
-
